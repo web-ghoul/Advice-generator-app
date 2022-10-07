50d0fd8f74be804ed3db697eb33b5ff70bb0d038
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2022-10-07 21:12:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const quote = document.querySelector("section .contain p")
+
+const num = document.querySelector("section .contain .head span")
+
+const myRequest = new Request("	https://api.adviceslip.com/advice")
+
+const diceBtn= document.querySelector("section .contain picture.dice")
+
+diceBtn.addEventListener("click",function(){
+    fetch(myRequest)
+  .then((response) => response.json())
+  .then((data) => {
+    quote.innerHTML = "\“ " + data.slip.advice+" \”"
+    num.innerHTML = "# "+data.slip.id
+  })
+  .catch(console.error);
+})
