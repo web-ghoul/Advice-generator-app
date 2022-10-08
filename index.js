7c6b396df89a2ec1c33439b40d8c7a7807ad7392
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2022-10-08 18:18:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const myRequest = new Request("	https://api.adviceslip.com/advice")
 
 const diceBtn= document.querySelector("section .contain picture.dice")
 
+const time = setTimeout(function(){
+  fetch(myRequest)
+      .then((response) => response.json())
+      .then((data) => {
+        quote.innerHTML = "\“ " + data.slip.advice+" \”"
+        num.innerHTML = "# "+data.slip.id
+      })
+      .catch(console.error);
+},100)
+
 diceBtn.addEventListener("click",function(){
     fetch(myRequest)
-  .then((response) => response.json())
-  .then((data) => {
-    quote.innerHTML = "\“ " + data.slip.advice+" \”"
-    num.innerHTML = "# "+data.slip.id
-  })
-  .catch(console.error);
+      .then((response) => response.json())
+      .then((data) => {
+        quote.innerHTML = "\“ " + data.slip.advice+" \”"
+        num.innerHTML = "# "+data.slip.id
+      })
+      .catch(console.error);
 })
